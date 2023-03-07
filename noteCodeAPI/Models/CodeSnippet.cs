5497bd6878c81591a4c7985b93f0bf0b77374471
Mosko54 <moussaid08@outlook.fr>
2023-03-07 13:34:32 +0100

    ajout du modele CodeSnippet

---
+++ noteCodeAPI/Models/CodeSnippet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace noteCodeAPI.Models
+{
+    [Table("code_snippet")]
+    public class CodeSnippet
+    {
+        private int id;
+        private string code;
+        private string description;
+
+        [Column("id")]
+        public int Id { get => id; set => id = value; }
+
+        [Column("code")]
+        public string Code { get => code; set => code = value ?? throw new ArgumentNullException(); }
+
+        [Column("description")]
+        public string Description { get => description; set => description = value; }
+    }
+}
